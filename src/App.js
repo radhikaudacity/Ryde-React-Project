36230d49978006498752e1626323d3280e13cacd
Radhika R <radhika_rb@hotmail.com>
2021-10-05 19:35:12 +0530

    lazy loading removed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Navigation from './components/navigation';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/home';
-// import ProjectDetail from './pages/projectDetail';
+import ProjectDetail from './pages/projectDetail';
 import TeamDetail from './pages/teamDetail';
 import About from './pages/about';
 import projectData from './assets/projectsData';
 
-const ProjectDetail = lazy(() => import('./pages/projectDetail'));
+//const ProjectDetail = lazy(() => import('./pages/projectDetail'));
 // const Home = lazy(() => import('./pages/home'));
 
 function App() {
@@ -22,28 +22,27 @@ function App() {
   return (
     <>
       <Navigation />
-      <Suspense fallback={<h2>Loading ...</h2>}>
-        <Switch>
-          <Route path='/home' exact>
-            <Home projectData={projectData} />
-          </Route>
-          <Route path='/team/:id'>
-            <TeamDetail />
-          </Route>
-          <Route path='/projectinfo/:id'>
-            <ProjectDetail projectData={projectData} />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/'>
-            <Redirect to='/home' />
-          </Route>
-          <Route path='*'>
-            <Redirect to='/home' />
-          </Route>
-        </Switch>
-      </Suspense>
+
+      <Switch>
+        <Route path='/home' exact>
+          <Home projectData={projectData} />
+        </Route>
+        <Route path='/team/:id'>
+          <TeamDetail />
+        </Route>
+        <Route path='/projectinfo/:id'>
+          <ProjectDetail projectData={projectData} />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/'>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='*'>
+          <Redirect to='/home' />
+        </Route>
+      </Switch>
     </>
   );
 }
